8e6855eb37fb2b8d5a704b2c8d5c5260c534583d
DIMaslov1904 <maslow.dmitry2000@ya.ru>
2023-08-16 13:25:05 +0700

    AdBlock вызов функции раньше объявления

--- ToolsMate[AdBlock].lua
+++ ToolsMate[AdBlock].lua
@@ -1,49 +1,50 @@
 script_name('ToolsMate[AdBlock]')
 script_author('DIMaslov1904')
-script_version("1.1.0")
+script_version("1.1.1")
 script_url("https://github.com/DIMaslov1904/ToolsMate")
-script_description('Р‘Р»РѕРєРёСЂРѕРІРєР° РІС‹РІРѕРґР° РІ С‡Р°С‚ СѓРєР°Р·Р°РЅРЅС‹С… СЃРѕРѕР±С‰РµРЅРёР№.')
+script_description('Блокировка вывода в чат указанных сообщений.')
 
 
--- РџРµСЂРµРјРµРЅРЅС‹Рµ
-local on = true     -- СЃРѕСЃС‚РѕСЏРЅРёРµ РїРѕ СѓРјРѕР»С‡Р°РЅРёСЋ
-local messages = {  -- РїСЂРё РЅР°Р»РёС‡РёРё РґР°РЅРЅС‹С… С„СЂР°Р· - СЃРѕРѕР±С‰РµРЅРёРµ РЅРµ РІС‹РІРѕРґРёС‚СЃСЏ РІ С‡Р°С‚
-  'РћР±СЉСЏРІР»РµРЅРёРµ:',
-  'Р РµРґР°РєС†РёСЏ News',
+-- Переменные
+local on = true     -- состояние по умолчанию
+local messages = {  -- при наличии данных фраз - сообщение не выводится в чат
+  'Объявление:',
+  'Редакция News',
   '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~',
-  'Р—Р°РґР°Р№С‚Рµ РІР°С€ РІРѕРїСЂРѕСЃ РІ РїРѕРґРґРµСЂР¶РєСѓ СЃРµСЂРІРµСЂР° - /ask',
-  'Р’СЃСЋ РёРЅС‚РµСЂРµСЃСѓСЋС‰СѓСЋ РІР°СЃ РёРЅС„РѕСЂРјР°С†РёСЋ РІС‹ РјРѕР¶РµС‚Рµ РїРѕР»СѓС‡РёС‚СЊ РЅР° СЃР°Р№С‚Рµ - samp-rp.ru',
-  'РРіСЂР°Р№С‚Рµ РІРјРµСЃС‚Рµ СЃ РјСѓР·С‹РєРѕР№ РѕС‚ РѕС„РёС†РёР°Р»СЊРЅРѕРіРѕ СЂР°РґРёРѕ Samp RolePlay - /music',
+  'Задайте ваш вопрос в поддержку сервера - /ask',
+  'Всю интересующую вас информацию вы можете получить на сайте - samp-rp.ru',
+  'Играйте вместе с музыкой от официального радио Samp RolePlay - /music',
 }
 
 
 function main()
-  -- Р—Р°РІРёСЃРёРјРѕСЃС‚Рё
+  -- Зависимости
   if not isSampLoaded() or not isSampfuncsLoaded() then return end
   while not isSampAvailable() do wait(0) end
 
   local isSampev, sampev = xpcall(require, function ()
-      sampAddChatMessage(script.this.name..' РІС‹РіСЂСѓР¶РµРЅ. Р‘РёР±Р»РёРѕС‚РµРєРё [SAMP.Lua] РЅРµ СѓСЃС‚Р°РЅРѕРІР»РµРЅС‹!', 0xD87093)
+      sampAddChatMessage(script.this.name..' выгружен. Библиотеки [SAMP.Lua] не установлены!', 0xD87093)
       thisScript():unload()
     end, 'samp.events')
   if not isSampev then return end
 
 
-  -- РРЅС‚РµРіСЂР°С†РёСЏ СЃ ToolsMate
+  -- Интеграция с ToolsMate
   EXPORTS.TAG_ADDONS = 'ToolsMate'
   EXPORTS.NAME_ADDONS = 'AdBlock'
   EXPORTS.URL_CHECK_UPDATE = 'https://raw.githubusercontent.com/DIMaslov1904/ToolsMate/main/version.json'
   EXPORTS.URL_GET_UPDATE = 'https://raw.githubusercontent.com/DIMaslov1904/ToolsMate/main/ToolsMate%5BAdBlock%5D.lua'
-  EXPORTS.run = ads
 
-
+  
   local function ads()
     local isNotify, notify = pcall(import, ('ToolsMate'))
     on = not on
-    local message = ('{%s}AdBlock. Р‘Р»РѕРєРёСЂРѕРІРєР° %s!'):format(on and '00FF00' or 'FF0000', on and 'РІРєР»СЋС‡С‘РЅР°' or 'РІС‹РєР»СЋС‡РµРЅР°')
+    local message = ('{%s}AdBlock. Блокировка %s!'):format(on and '00FF00' or 'FF0000', on and 'включёна' or 'выключена')
     if isNotify then notify.addNotify( message, 5)
     else sampAddChatMessage(message, 0xFF0000) end
   end
+
+  EXPORTS.run = ads
 
   function sampev.onServerMessage(_, text)
     if on==true then for _, mess in ipairs(messages) do
